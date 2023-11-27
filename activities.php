a5bfa3170d992964d9b37b972f9cf6272af9e8fc
Huzaifa-Faizee <96124697+Huzaifa-Faizee@users.noreply.github.com>
2023-11-27 11:33:29 +0530

    button and css

---
+++ activities.php
@@ -0,0 +1,15 @@
+<?php
+$activities = [
+    ["Read a book"],
+    ["Go for a walk"],
+    ["Try a new recipe"],
+    ["Learn a new language"],
+    ["Start a DIY project"],
+    ["Write in a journal"],
+    ["Do a workout or exercise"],
+    ["Take up photography"],
+    ["Learn to code"],
+    ["Explore your town or city"],
+    ["Meditate or practice mindfulness"],
+];
+?>
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+include("activities.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,33 +22,19 @@
 
 <body>
     <h1>Fill The Free Time</h1>
-    <p id="activity">Not sure what to do in your free time? Click the button for a suggestion!</p>
+    <p class="activity">Not sure what to do in your free time? Click the button for a suggestion!</p>
     <form action="#" method="post">
-        <button name="submit" type="submit">Get Activity</button>
+        <button name="submit" type="submit" class="button-74">Get Activity</button>
     </form>
     <?php
     if (isset($_POST["submit"])) {
-        $activities = [
-            "Read a book",
-            "Go for a walk",
-            "Try a new recipe",
-            "Learn a new language",
-            "Start a DIY project",
-            "Practice a musical instrument",
-            "Write in a journal",
-            "Do a workout or exercise",
-            "Take up photography",
-            "Join a local club or group",
-            "Learn to code",
-            "Explore a nearby town or city",
-            "Volunteer for a cause you care about",
-            "Meditate or practice mindfulness",
-            "Watch a documentary"
-        ];
         $i = mt_rand(0, count($activities) - 1);
         ?>
         <div class="content">
-            <?= $activities[$i] ?>
+            <span class="fade-in">How About? </span>
+            <span class="delayed-fade-in">
+                <?= $activities[$i][0] ?>
+            </span>
         </div>
         <?php
     }
