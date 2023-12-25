12ae3f98ebb6c92fb58bc472a28840c42e766571
Huzaifa-Faizee <96124697+Huzaifa-Faizee@users.noreply.github.com>
2023-12-25 23:09:35 +0530

    changes

--- activities.php
+++ activities.php
@@ -3,14 +3,14 @@ $activities = [
     ["Read a book","Reading","https://markhampubliclibrary.ca/blogs/post/top-10-benefits-of-reading-for-all-ages/"],
     ["Go for a walk","Walking","https://www.healthline.com/health/benefits-of-walking"],
     // ["Try a new recipe"],
-    // ["Learn a new language"],
-    // ["Start a DIY project"],
-    // ["Write in a journal"],
+    ["Learn a new language","Language","https://www.telegraphindia.com/edugraph/career/10-benefits-of-learning-a-new-language/cid/1883699"],
+    ["Start a DIY project","DIY","https://totalpond.com/blogs/water-gardening/benefits-of-crafting-and-diy-projects"],
+    ["Write in a journal","Journal","https://healthy.kaiserpermanente.org/health-wellness/healtharticle.7-benefits-of-keeping-a-journal"],
     // ["Do a workout or exercise"],
     // ["Take up photography"],
-    // ["Learn to code"],
-    // ["Explore your town or city"],
-    // ["Meditate or practice mindfulness"],
+    ["Learn to code","Code","https://www.fdmgroup.com/blog/7-benefits-of-learning-to-code/"],
+    ["Explore a your/new town or city","Explore","https://www.pushdoctor.co.uk/exercise/8-health-benefits-of-exploring-new-places"],
+    ["Meditate or practice mindfulness","Meditate","https://health.ucdavis.edu/blog/cultivating-health/10-health-benefits-of-meditation-and-how-to-focus-on-mindfulness-and-compassion/2022/12"],
 ];
 $benefits = [
     [
@@ -28,7 +28,56 @@ $benefits = [
         "Walking may reduce your risk for developing a cold or the flu.",
         "Improve your mood",
         "Walking can help your mental health. StudiesTrusted Source show it can help reduce anxiety, depression, and a negative mood. It can also boost self-esteem and reduce symptoms of social withdrawal."
+    ],
+    [
+        "Helps to connect with people",
+        "In both their personal and professional life, bilinguals have the remarkable opportunity to communicate with a wider range of people.",
+        "Increases the size, health, and efficiency of your brain",
+        "In order to make sense of the various linguistic systems, the brain has to put extra effort into recognizing, deciphering and communicating in the new language. This exercise is healthy for the brain. Thus, learning a second language helps it function more effectively.",
+        "Enhances memory skills",
+        "Babies raised in a bilingual environment have better working memories than those raised in a monolingual environment. They are therefore more adept in reading, mental math, and many other crucial skills."
+    ],
+    [
+        "Learning and acquiring a new Skill",
+        "Completing a craft or DIY project is a fun way to learn something new and gives you one of the most satisfying feelings because it helps to foster a sense of accomplishment since it produces a tangible result. You also now have a new useful skill that can be for your enjoyment or turned into an additional source of income.",
+        "Saves Money",
+        "Crafting and DIY often costs less money than buying mass manufactured items. ",
+        "Creates an outlet for emotion",
+        "Psychology Today had an interesting piece about how crafting and DIY projects are used for managing emotion. For a long time, art has been used as a coping mechanism to manage difficult feelings and trauma."
+    ],
+    [
+        "Achieve goals",
+        "When you use your journal to write down your goals, you can keep better track of your intentions. This will help you stay accountable and serve as a reminder of what you need to do to reach your goals. Be sure to get specific.",
+        "Track progress and growth",
+        "If you make journaling a regular habit, you can see how much progress or growth you’ve made by revisiting previous entries.",
+        "Gain self-confidence",
+        "Seeing your progress can also give you a serious confidence boost. You can feel proud looking back at the challenges you faced and seeing how far you’ve come."
+    ],
+    [
+        "Enhance problem-solving skills",
+        "Coding can help you improve your logical thinking skills by allowing you to see problems from a new perspective. Complex coding projects are made up of smaller tasks and by breaking down the problem and adopting a methodological way of thinking, you can tackle any challenge. ",
+        "Can improve career prospects",
+        "Coding is an in-demand skill across industries and offers excellent earning potential with a wide variety of career choices, such as RPA, Software Development and Technical Operations. Computer Programming also offers vast opportunities for career progression into managerial and senior positions, allowing you to keep learning on the job and expand your skillset.",
+        "Can help you understand technology",
+        "Technology is everywhere in the modern world and continues to develop at an astonishing rate. Learning to code will give you the tools you need to go on and be part of shaping the future."
+    ],
+    [
+        "It's great for your mental health",
+        "One of the main reasons for getting away from it all is to de-stress. You can relax by the pool or take in some fresh countryside air. Whatever it is, studies suggest it only takes a day or two of switching off for you to leave your troubles behind.",
+        "You'll get more exercise",
+        "Whether you're hiking in the hills or exploring a new city on foot (the best way to do it), you can bet that your step count will go through the roof.The amount of walking you'll do while you're away will really help your cardiovascular health, especially if your normal day involves sitting behind a desk.",
+        "You can boost your immune system",
+        "Travel can expose you to a range of new environments, complete with diseases you might not get back home. The good news is that you'll be vaccinated against the most dangerous ones and when it comes to the rest, your clever old immune system is ready to step in."
+    ],
+    [
+        "Reduced stress",
+        "Meditation may decrease stress. It can also improve  symptoms of stress-related conditions",
+        "Improved memory",
+        "Better focus through regular meditation may increase memory and mental clarity. These benefits can help fight age-related memory loss and dementia.",
+        "Better sleep",
+        "Meditation can shorten the time it takes to fall asleep and improve sleep quality."
     ]
+
 ]
 
 
